f8c246aee8c64741cc3c6f25791c681b578427ef
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2023-02-01 12:52:12 +0100

    Hecho avances

--- Servicios/Web/GenerarHTML.cs
+++ Servicios/Web/GenerarHTML.cs
@@ -34,7 +34,7 @@ namespace RestReviewV2.Servicios.Web
 
             
             // Escribir el HTML en un archivo
-            File.WriteAllText("data.html", html);
+            File.WriteAllText("./Assets/web/webplantilla.html", html);
         }
     }
 }

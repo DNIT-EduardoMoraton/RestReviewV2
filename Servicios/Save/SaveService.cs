21deeb67e22822cbcf3f8db7846c7754437b0714
Jose Antonio Guerrero Vidal <90720873+joseantoniogv@users.noreply.github.com>
2023-02-01 22:17:04 +0100

    Añado Save
    Falta probarlo y añadir alguna cosa

---
+++ Servicios/Save/SaveService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestReviewV2.Servicios.GuardarHTML
+{
+    class SaveService
+    {
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        saveFileDialog.Filter = "HTML Files (*.html)|*.html|All Files (*.*)|*.*";
+    saveFileDialog.DefaultExt = ".html";
+
+    if (saveFileDialog.ShowDialog() == true)
+    {
+        string htmlContent = GenerateHTML();
+
+        using (StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName))
+        {
+            streamWriter.Write(htmlContent);
+        }
+    }
+}

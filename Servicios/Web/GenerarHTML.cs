f9ec59ce64e3dab80b31e2707a8d8b38828293eb
Jose Antonio Guerrero Vidal <ja.guerrero@iesdoctorbalmis.com>
2023-02-01 12:26:02 +0100

    generarhtml

---
+++ Servicios/Web/GenerarHTML.cs
@@ -0,0 +1,40 @@
+﻿using GestorRestReview.BD;
+using GestorRestReview.Modelo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestReviewV2.Servicios.Web
+{
+    class GenerarHTML
+    {
+        private SeccionService seccion = new SeccionService();
+
+        public GenerarHTML()
+        {
+        }
+
+        public void GenerateHTML()
+        {
+            // Iniciar la cadena HTML
+            string html = "<html><body><h1>Revista</h1>";
+
+
+            foreach (Seccion data in seccion.GetAll())
+            {
+                html += "<h2>Seccion</h2>" +
+                        "<p>Nombre: " + data.Nombre + "</p>" +
+                        "<p>Descripcion: " + data.Descripcion + "</p>";
+            }
+
+            html += "</body></html>";
+
+            
+            // Escribir el HTML en un archivo
+            File.WriteAllText("data.html", html);
+        }
+    }
+}

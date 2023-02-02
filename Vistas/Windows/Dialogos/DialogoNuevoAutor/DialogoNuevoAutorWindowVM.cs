e2c6b8d2d036234e4aaee511a3d53e3698ea9100
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2023-02-02 14:50:21 +0100

    avances en dialogo

--- Vistas/Windows/Dialogos/DialogoNuevoAutor/DialogoNuevoAutorWindowVM.cs
+++ Vistas/Windows/Dialogos/DialogoNuevoAutor/DialogoNuevoAutorWindowVM.cs
@@ -1,4 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GestorRestReview.BD;
+using GestorRestReview.Modelo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,32 @@ namespace RestReviewV2.Vistas.Windows.Dialogos.DialogoNuevoAutor
 {
     class DialogoNuevoAutorWindowVM : ObservableRecipient
     {
+        private Autor autorActual;
+
+        public Autor AutorActual
+        {
+            get { return autorActual; }
+            set { SetProperty(ref autorActual, value); }
+        }
+
+        // Commands
+        private RelayCommand crearCommand;
+
+        // Servicios
+        private AutoresService servicioAutores;
         public DialogoNuevoAutorWindowVM()
         {
+            servicioAutores = new AutoresService();
+            AutorActual = new Autor();
 
+            ManejadorCommands();
         }
+
+        private void ManejadorCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public 
     }
 }

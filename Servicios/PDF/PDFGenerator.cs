2b6fbaf265e07cb9fc51adb707bf94025a01ab35
Jose Antonio Guerrero Vidal <ja.guerrero@iesdoctorbalmis.com>
2023-02-02 11:39:07 +0100

    Se viene

---
+++ Servicios/PDF/PDFGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quest.Lib.PDF;
+
+namespace RestReviewV2.Servicios.PDF
+{
+    class PDFGenerator
+    {
+
+    }
+}

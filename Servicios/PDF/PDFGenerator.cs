4d33b0389a739d7025c7720cc2f69327a439f8f1
Jose Antonio Guerrero Vidal <ja.guerrero@iesdoctorbalmis.com>
2023-02-02 13:36:39 +0100

    Creo generador PDF

--- Servicios/PDF/PDFGenerator.cs
+++ Servicios/PDF/PDFGenerator.cs
@@ -3,12 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Quest.Lib.PDF;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Previewer; 
+using QuestPDF.Infrastructure;
 
 namespace RestReviewV2.Servicios.PDF
 {
     class PDFGenerator
     {
+        public PDFGenerator()
+        {
 
+            Document.Create(document =>
+                {
+                    document.Page(page =>
+                    {
+                        page.Margin(1, Unit.Inch);
+                        page.Header()
+                        .Text("Hello PDF")
+                        .FontSize(48)
+                        .FontColor(Colors.Blue.Darken2)
+                        .SemiBold();
+                        page.Content()
+                        .Column(column =>
+                        {
+                            column.Spacing(0.5f, Unit.Inch);
+
+                            column.Item()
+                            .Text(Placeholders.LoremIpsum())
+                            .FontSize(18);
+
+                            column.Item()
+                            .AspectRatio(16 / 9f).Image(Placeholders.Image);
+                        });
+
+                        page.Footer()
+                        .AlignCenter()
+                        .Text(text =>
+                        {
+                            text.DefaultTextStyle(x => x.FontSize(18));
+                            text.CurrentPageNumber();
+                            text.Span("/");
+                            text.TotalPages();
+                        });
+                    });
+                }).GeneratePdf("output.pdf");
+        }
     }
 }
+      
+
--- Vistas/UserControls/HomeWebPreview/HomeWebPreviewUserControlVM.cs
+++ Vistas/UserControls/HomeWebPreview/HomeWebPreviewUserControlVM.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using GestorRestReview.Servicios;
 using RestReviewV2.Servicios.GuardarHTML;
 using RestReviewV2.Servicios.Moderacion;
+using RestReviewV2.Servicios.PDF;
 using RestReviewV2.Servicios.Web;
 using System;
 using System.Collections.Generic;
@@ -48,7 +49,8 @@ namespace GestorRestReview.Vistas.UserControls.HomeWebPreview
 
         private void InicioPorDefecto()
         {
-            servicioAlerta.MessageBoxError(new Moderator().ModerarTexto("hijo de puta"));
+            PDFGenerator pdf = new PDFGenerator();
+            //servicioAlerta.MessageBoxError(new Moderator().ModerarTexto("hijo de puta"));
             htmlService.GenerateHTML();
             //HTMLRuta = "file:///" + Path.GetFullPath(htmlService.getPreview()); // Aqui solamente poner la ruta del archivo temporal para la pagina web 
             HTMLRuta = "www.google.es";

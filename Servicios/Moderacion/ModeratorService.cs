2c58d0f8f76114726eb4f94080381165c986ce0b
Jose Antonio Guerrero <ja.guerrero@iesdoctorbalmis.es>
2023-02-17 00:35:29 +0100

    agrego documentacion

--- Servicios/Moderacion/ModeratorService.cs
+++ Servicios/Moderacion/ModeratorService.cs
@@ -15,20 +15,29 @@ using System.Threading.Tasks;
 
 namespace RestReviewV2.Servicios.Moderacion
 {
+    /// <summary>
+    /// Servicio que interactúa con la API de Content Moderator de Azure.
+    /// </summary>
     class ModeratorService : ObservableObject
     {
         private readonly string _baseUrl = "https://restmodreview.cognitiveservices.azure.com/contentmoderator/";
         private readonly string _subscriptionKey = "e2ea035cffd64e74bf391609868d1faf";
 
-
-
         private AlertaServicio servicioAlerta;
 
+        /// <summary>
+        /// Constructor que inicializa la instancia del servicio de alertas.
+        /// </summary>
         public ModeratorService()
         {
             servicioAlerta = new AlertaServicio();
         }
 
+        /// <summary>
+        /// Método que realiza la moderación de un texto.
+        /// </summary>
+        /// <param name="text">Texto a moderar.</param>
+        /// <returns>Lista de términos moderados.</returns>
         public async Task<List<string>> Moderate(string text)
         {
             var client = new RestClient(_baseUrl + "moderate/v1.0/ProcessText/Screen");
@@ -37,27 +46,31 @@ namespace RestReviewV2.Servicios.Moderacion
             request.AddHeader("Content-Type", "text/plain");
             request.AddParameter("text/plain", text, ParameterType.RequestBody);
 
-
-
             APIRootMod res = await LaunchAzureApi<APIRootMod>(client, request);
             return res.Terms.Select(t => t.Term).ToList();
         }
 
+        /// <summary>
+        /// Método que realiza la moderación de un texto.
+        /// </summary>
+        /// <param name="text">Texto a moderar.</param>
+        /// <param name="id">Identificador del texto.</param>
+        /// <returns>Lista de términos moderados.</returns>
         public List<string> Moderate(string text, string id)
         {
             return null;
         }
 
-
-        // Gestion de listas
-
+        /// <summary>
+        /// Método que obtiene todas las listas de moderación disponibles.
+        /// </summary>
+        /// <returns>Colección observable de listas de moderación.</returns>
         public async Task<ObservableCollection<ListaModeracion>> GetAllLists()
         {
             ObservableCollection<ListaModeracion> lista = new ObservableCollection<ListaModeracion>();
             var client = new RestClient(_baseUrl + "lists/v1.0/termlists");
             var request = new RestRequest(Method.GET);
             request.AddHeader("Ocp-Apim-Subscription-Key", _subscriptionKey);
-            
 
             List<APIRootListMod> listAPI = await LaunchAzureApi<List<APIRootListMod>>(client, request);
             lista = new ObservableCollection<ListaModeracion>(listAPI
@@ -65,45 +78,14 @@ namespace RestReviewV2.Servicios.Moderacion
                 .ToList());
 
             return lista;
-
-        }
-
-        public async Task<ObservableCollection<string>> GetTerms(string id)
-        {  
-            var client = new RestClient(_baseUrl + $"lists/v1.0/termlists/{id}/terms");
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("Ocp-Apim-Subscription-Key", _subscriptionKey);
-            request.AddParameter("language", "spa");
-
-            APIRootList rootList = await LaunchAzureApi<APIRootList>(client, request).ConfigureAwait(true);
-
-            List<string> list = new List<string>();
-
-            if (list!=null)
-            {
-                rootList.Data.Terms.ForEach(t => list.Add(t.Term));
-            }
-            
-
-            return new ObservableCollection<string>(list);
-        }
-
-        
-        public bool AddTerm(String listId, String term)
-        {
-            var client = new RestClient(_baseUrl + $"lists / v1.0 / termlists /{listId}/ terms /{term}");
-            var request = new RestRequest(Method.POST);
-            request.AddParameter("language", "spa");
-
-            RestResponse response = (RestResponse) client.Execute(request);
-            if (((int)response.StatusCode) == 201)
-            {
-                return true;
-            }
-            return false;
         }
 
 
+        /// <summary>
+        /// Método que obtiene los términos de una lista de moderación.
+        /// </summary>
+        /// <param name="id">Identificador de la lista.</param>
+        /// <returns>Colección observable de términos de la lista.</returns>
         private async Task<T> LaunchAzureApi<T>(RestClient cli, RestRequest req)
         {
             T res = default(T);
